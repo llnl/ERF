c55a8dae863a822ba52fded39bb8a8f2e4dae6af
Cole Kendrick <kendrick6@llnl.gov>
2025-05-28 07:22:44 -0700

    Make sure qheating rates are defined if using QRAD forcing

--- Source/ERF_MakeNewArrays.cpp
+++ Source/ERF_MakeNewArrays.cpp
@@ -328,7 +328,7 @@ ERF::init_stuff (int lev, const BoxArray& ba, const DistributionMapping& dm,
     //*********************************************************
     // Radiation heating source terms
     //*********************************************************
-    if (solverChoice.rad_type != RadiationType::None || solverChoice.lsm_type != LandSurfaceType::None)
+    if (solverChoice.rad_type != RadiationType::None || solverChoice.lsm_type != LandSurfaceType::None || solverChoice.do_radiation)
     {
         qheating_rates[lev] = std::make_unique<MultiFab>(ba, dm, 2, ngrow_state);
         qheating_rates[lev]->setVal(0.);
